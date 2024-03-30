7da25b647da8fa7101539300b22ca3f7a67907a1
Kuan-Lun Wang <kuan-lun@klwang.tw>
2024-03-30 22:00:50 +0800

    設定好 setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="hentaidb",
+    version="0.0.1",
+    author="Kuan-Lun Wang",
+    author_email="kuan-lun@klwang.tw",
+    description="A simple Hentai at Home database",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=["hentaidb"],
+    install_requires=[],
+)
+
+# Path: hentaidb/__init__.py
