38fdb7a35a9fd5b3149c5e5a13ebfd23259a552b
Kuan-Lun Wang <kuan-lun@klwang>
2024-06-22 20:22:45 +0800

    將 CBZ 壓縮改為強制指定 LANCZOS 算法。

--- src/h2hdb/compress_gallery_to_cbz.py
+++ src/h2hdb/compress_gallery_to_cbz.py
@@ -29,10 +29,7 @@ def compress_image(image_path: str, output_path: str, max_size: int) -> None:
             else:
                 max_width = image.width
                 max_height = image.height
-        try:
-            image.thumbnail((max_width, max_height))
-        except ValueError:
-            image.thumbnail((max_width, max_height), resample=Image.NEAREST)  # type: ignore
+        image.thumbnail((max_width, max_height), resample=Image.LANCZOS)  # type: ignore
         image.save(output_path, image.format)
 
 
