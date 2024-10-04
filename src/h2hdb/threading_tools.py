9fee6823c158ffe3d2a48b11dbada390d2537932
Kuan-Lun Wang <kuan-lun@klwang.tw>
2024-10-04 10:51:27 +0800

    優化 threads 的操作。

--- src/h2hdb/threading_tools.py
+++ src/h2hdb/threading_tools.py
@@ -5,8 +5,6 @@ from multiprocessing import cpu_count
 from multiprocessing.pool import Pool
 from contextlib import ExitStack
 
-from .logger import logger
-
 POOL_CPU_LIMIT = max(cpu_count() - 2, 1)
 
 # MAX_IO_SEMAPHORE = threading.Semaphore(5)
@@ -15,57 +13,52 @@ KOMGA_SEMAPHORE = threading.Semaphore(5)
 SQL_SEMAPHORE = threading.Semaphore(10)
 
 
-class BackgroundTaskThread(Thread, metaclass=ABCMeta):
+def wrap_thread_target_with_semaphores(target, get_semaphores):
+    def wrapper(*args, **kwargs):
+        with ExitStack() as stack:
+            for semaphore in get_semaphores():
+                stack.enter_context(semaphore)
+            target(*args, **kwargs)
+
+    return wrapper
+
+
+class ThreadsList(list, metaclass=ABCMeta):
     @abstractmethod
     def get_semaphores(self) -> list[threading.Semaphore]:
         pass
 
-    def start(self) -> None:
-        with ExitStack() as stack:
-            for semaphore in self.get_semaphores():
-                stack.enter_context(semaphore)
-            try:
-                super().start()
-            except BaseException as e:
-                logger.error(f"Error in background task: {e}")
-
-
-class ThreadsList(list, metaclass=ABCMeta):
-    class LocalBackgroundTaskThread(BackgroundTaskThread, metaclass=ABCMeta):
-        pass
-
-    def join_all(self: list[BackgroundTaskThread]):
-        for thread in self:
-            thread.join()
-
     def append(self, target, args):
-        thread = self.LocalBackgroundTaskThread(target=target, args=args)
-        thread.start()
+        thread = Thread(
+            target=wrap_thread_target_with_semaphores(target, self.get_semaphores),
+            args=args,
+        )
         super().append(thread)
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.join_all()
+        for thread in self:
+            with ExitStack() as stack:
+                for semaphore in self.get_semaphores():
+                    stack.enter_context(semaphore)
+                thread.start()
 
 
 # class CBZThreadsList(ThreadsList):
-#     class LocalBackgroundTaskThread(BackgroundTaskThread):
-#         def get_semaphores(self) -> list[threading.Semaphore]:
-#             return [CBZ_IO_SEMAPHORE, MAX_IO_SEMAPHORE]
+#     def get_semaphores(self) -> list[threading.Semaphore]:
+#         return [CBZ_IO_SEMAPHORE, MAX_IO_SEMAPHORE]
 
 
 class KomgaThreadsList(ThreadsList):
-    class LocalBackgroundTaskThread(BackgroundTaskThread):
-        def get_semaphores(self) -> list[threading.Semaphore]:
-            return [KOMGA_SEMAPHORE]
+    def get_semaphores(self) -> list[threading.Semaphore]:
+        return [KOMGA_SEMAPHORE]
 
 
 class SQLThreadsList(ThreadsList):
-    class LocalBackgroundTaskThread(BackgroundTaskThread):
-        def get_semaphores(self) -> list[threading.Semaphore]:
-            return [SQL_SEMAPHORE]
+    def get_semaphores(self) -> list[threading.Semaphore]:
+        return [SQL_SEMAPHORE]
 
 
 def run_in_parallel(fun, args: list[tuple]) -> list:
