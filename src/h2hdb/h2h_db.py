758e430422e57b0b9ce7e67ceb65e04656c0f948
Kuan-Lun Wang <kuan-lun@klwang.tw>
2025-03-23 14:43:35 +0800

    將 duplicated_hash_values_by_count_artist_ratio 所需的暫時預覽表隱藏。

--- src/h2hdb/h2h_db.py
+++ src/h2hdb/h2h_db.py
@@ -2052,62 +2052,46 @@ class H2HDB(
             match self.config.database.sql_type.lower():
                 case "mysql":
                     query = """
-                        CREATE VIEW IF NOT EXISTS duplicated_files_hashs_sha512 AS 
-                            SELECT db_file_id, db_hash_id
+                        CREATE VIEW IF NOT EXISTS duplicated_hash_values_by_count_artist_ratio AS WITH duplicated_files_hashs_sha512 AS (
+                            SELECT db_file_id,
+                                db_hash_id
                             FROM files_hashs_sha512
                             GROUP BY db_hash_id
-                            HAVING COUNT(*) >= 3;
-                        """
-            connector.execute(query)
-
-            match self.config.database.sql_type.lower():
-                case "mysql":
-                    query = """
-                        CREATE VIEW IF NOT EXISTS duplicated_db_dbids AS 
-                            SELECT
-                                galleries_dbids.db_gallery_id AS db_gallery_id,
+                            HAVING COUNT(*) >= 3
+                        ),
+                        duplicated_db_dbids AS (
+                            SELECT galleries_dbids.db_gallery_id AS db_gallery_id,
                                 files_dbids.db_file_id AS db_file_id,
                                 duplicated_files_hashs_sha512.db_hash_id AS db_hash_id,
                                 galleries_tag_pairs_dbids.tag_value AS artist_value
                             FROM duplicated_files_hashs_sha512
-                            LEFT JOIN files_hashs_sha512
-                                ON duplicated_files_hashs_sha512.db_hash_id = files_hashs_sha512.db_hash_id
-                            LEFT JOIN files_dbids
-                                ON files_hashs_sha512.db_file_id = files_dbids.db_file_id
-                            LEFT JOIN galleries_dbids
-                                ON files_dbids.db_gallery_id = galleries_dbids.db_gallery_id
-                            LEFT JOIN galleries_tags
-                                ON galleries_dbids.db_gallery_id = galleries_tags.db_gallery_id
-                            LEFT JOIN galleries_tag_pairs_dbids
-                                ON galleries_tags.db_tag_pair_id = galleries_tag_pairs_dbids.db_tag_pair_id
-                            WHERE galleries_tag_pairs_dbids.tag_name = 'artist';
-                        """
-            connector.execute(query)
-
-            match self.config.database.sql_type.lower():
-                case "mysql":
-                    query = """
-                        CREATE VIEW IF NOT EXISTS duplicated_count_artists_by_db_gallery_id AS
-                            SELECT
-                                COUNT(DISTINCT artist_value) AS artist_count,
+                                LEFT JOIN files_hashs_sha512 ON duplicated_files_hashs_sha512.db_hash_id = files_hashs_sha512.db_hash_id
+                                LEFT JOIN files_dbids ON files_hashs_sha512.db_file_id = files_dbids.db_file_id
+                                LEFT JOIN galleries_dbids ON files_dbids.db_gallery_id = galleries_dbids.db_gallery_id
+                                LEFT JOIN galleries_tags ON galleries_dbids.db_gallery_id = galleries_tags.db_gallery_id
+                                LEFT JOIN galleries_tag_pairs_dbids ON galleries_tags.db_tag_pair_id = galleries_tag_pairs_dbids.db_tag_pair_id
+                            WHERE galleries_tag_pairs_dbids.tag_name = 'artist'
+                        ),
+                        duplicated_count_artists_by_db_gallery_id AS(
+                            SELECT COUNT(DISTINCT artist_value) AS artist_count,
                                 db_gallery_id
                             FROM duplicated_db_dbids
                             GROUP BY db_gallery_id
-                        """
-            connector.execute(query)
-
-            match self.config.database.sql_type.lower():
-                case "mysql":
-                    query = """
-                        CREATE VIEW IF NOT EXISTS duplicated_hash_values_by_count_artist_ratio AS
-                            SELECT files_hashs_sha512_dbids.hash_value AS hash_value
+                        ),
+                        duplicated_count_artists_by_db_gallery_id AS(
+                            SELECT COUNT(DISTINCT artist_value) AS artist_count,
+                                db_gallery_id
                             FROM duplicated_db_dbids
-                            LEFT JOIN duplicated_count_artists_by_db_gallery_id
-                                ON duplicated_db_dbids.db_gallery_id = duplicated_count_artists_by_db_gallery_id.db_gallery_id
-                            LEFT JOIN files_hashs_sha512_dbids
-                                ON duplicated_db_dbids.db_hash_id = files_hashs_sha512_dbids.db_hash_id
-                            GROUP BY duplicated_db_dbids.db_hash_id
-                            HAVING COUNT(DISTINCT duplicated_db_dbids.artist_value)/MAX(duplicated_count_artists_by_db_gallery_id.artist_count) > 2
+                            GROUP BY db_gallery_id
+                        )
+                        SELECT files_hashs_sha512_dbids.hash_value AS hash_value
+                        FROM duplicated_db_dbids
+                            LEFT JOIN duplicated_count_artists_by_db_gallery_id ON duplicated_db_dbids.db_gallery_id = duplicated_count_artists_by_db_gallery_id.db_gallery_id
+                            LEFT JOIN files_hashs_sha512_dbids ON duplicated_db_dbids.db_hash_id = files_hashs_sha512_dbids.db_hash_id
+                        GROUP BY duplicated_db_dbids.db_hash_id
+                        HAVING COUNT(DISTINCT duplicated_db_dbids.artist_value) / MAX(
+                                duplicated_count_artists_by_db_gallery_id.artist_count
+                            ) > 2
                         """
             connector.execute(query)
 
@@ -2452,9 +2436,9 @@ class H2HDB(
         self._create_galleries_titles_table()
         self._create_upload_account_table()
         self._create_galleries_comments_table()
-        self._create_galleries_infos_view()
         self._create_todelete_names_view()
         self._create_files_names_table()
+        self._create_galleries_infos_view()
         self._create_galleries_files_hashs_tables()
         self._create_gallery_image_hash_view()
         self._create_removed_galleries_gids_table()
